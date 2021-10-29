819a69c0677b7daada65564fef7ee63d76bdcb60
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-10-29 18:38:22 +0530

    darktheme comments

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,11 +3,11 @@ export const darkTheme = {
   // surface: "#e1e1e1",
   surface: "rgba(128, 128, 128, 0.16)",
   primary: "#03dac6",
-  secondary: "lightseagreen",
+  secondary: "#20b2aa", // lightseagreen
   on_background: "#ffffff",
   on_surface_title: "#ffffff",
   on_surface_subtitle: "#b8bec3",
-  on_primary: "#121212",
-  on_secondary: "#121212",
+  on_primary: "#121212", // almost black
+  on_secondary: "#121212", // almost black
   status_bar: "light",
 };
