cd4819ce79567c96d23dbe8f6f17dd0ceb365a58
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-11-02 20:02:33 +0530

    reconfig sentry

--- app.config.js
+++ app.config.js
@@ -3,5 +3,18 @@ import "dotenv/config";
 export default ({ config }) => {
   return {
     ...config,
+    plugins: ["sentry-expo"],
+    hooks: {
+      postPublish: [
+        {
+          file: "sentry-expo/upload-sourcemaps",
+          config: {
+            organization: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+          },
+        },
+      ],
+    },
   };
 };
