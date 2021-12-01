65f4332be5e0e01196201198905eb1ec5fb018b3
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-12-01 12:37:31 +0530

    resize contain

--- src/utils/feedPlayer.js
+++ src/utils/feedPlayer.js
@@ -30,7 +30,7 @@ const VideoPlayer = ({
       <Video
         ref={videoRef}
         style={[styles.video, { height: VIDEO_HEIGHT }]}
-        resizeMode="cover"
+        resizeMode="contain"
         useNativeControls={false}
         onPlaybackStatusUpdate={(status) => {
           if (status.error) {
