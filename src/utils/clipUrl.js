630c6404c90d13c5c22228d50dc7c96eaa060cff
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-12-27 10:54:11 +0530

    clip default 720p 7

--- src/utils/clipUrl.js
+++ src/utils/clipUrl.js
@@ -2,6 +2,6 @@ export const clipUrlByNetSpeed = (templateUrl) => {
   let videoUri = templateUrl;
   // TODO decide based on internet speed
   videoUri = videoUri.replace("{}", "720");
-  videoUri = videoUri.replace("{}", "8");
+  videoUri = videoUri.replace("{}", "7");
   return videoUri;
 };
