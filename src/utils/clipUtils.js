6264566893402946445b8f96f0b62736de11b85f
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-02-11 15:26:36 +0530

    rm unused code

--- src/utils/clipUtils.js
+++ src/utils/clipUtils.js
@@ -1,11 +1,6 @@
-import { Dimensions } from "react-native";
-
-const screenWidth = Dimensions.get("window").width;
-const screenHeight = Dimensions.get("window").height;
-
 export const clipUrlByNetSpeed = (templateUrl) => {
   let videoUri = templateUrl;
   // TODO decide based on internet speed
-  videoUri = videoUri.replace("{}", "3");
+  videoUri = videoUri.replace("{}", "2");
   return videoUri;
 };
